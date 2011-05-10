644555179af6169304e00c427e4351032fe4289e
Sebastian Motraghi <sebastian.m@gmail.com>
2011-05-10 09:22:30 +0100

    Revert "Added a common_oauth pod which should be used by other pods requiring oauth functionality, or even directly."
    This reverts commit 158b89a696564afc587068973ca661e06800fbb8.

--- pods/core_oauth_connect/methods.php
+++
@@ -1,9 +0,0 @@
-<?
-    function common_oauth_settings() { 
-	    return array(
-		    'linkedin_api' => 'LinkedIn API Key',
-		    'linkedin_secret' => 'LinkedIn API Secret',
-		    'twitter_api' => 'Twitter API Key',
-		    'twitter_secret' => 'Twitter API Secret'
-	    );
-    }
