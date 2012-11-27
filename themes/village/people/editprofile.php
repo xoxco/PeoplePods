1ed06130dab670d1f3d91cc0fc6d6341d2906891
Nickolas Nikolic <me@nickolasnikolic.com>
2012-11-27 17:00:42 -0600

    Unsetting some structure in the themes so that bootstrap can take over styling; more to come

--- themes/village/people/editprofile.php
+++ themes/village/people/editprofile.php
@@ -24,45 +24,56 @@
 		<?php } // if unverified ?>
 		
 		<form id="edit_profile" method="post" action="<?php $POD->siteRoot(); ?>/editprofile"  class="valid" enctype="multipart/form-data">
-		
-			<p class="input"><label for="nick">My Username:</label>
-			<input class="required text"  maxlength="20" name="nick" id="nick" value="<?php $user->htmlspecialwrite('nick'); ?>"></p>	
-
-			<p class="input"><label for="email">My Email:</label>
-			<input class="required email text" name="email" id="email" value="<?php $user->htmlspecialwrite('email'); ?>"></p>
-	
-			<p class="input"><label for="photo">My Picture:</label>
-			<input name="img" type="file" id="img">
-				<?php if ($img = $user->files()->contains('file_name','img')) { ?>
-					<div id="file<?php echo $img->id; ?>" class="file">
-						<a href="<?php echo $img->original_file; ?>"><img src="<?php $img->write('thumbnail'); ?>" /></a>
-						<a href="#deleteFile" data-file="<?php echo $img->id;?>">Delete</a>
-					</div>
+          <label for="nick">My Username:</label>
+          <input maxlength="20" name="nick" id="nick" value="<?php $user->htmlspecialwrite('nick'); ?>">
+          <label for="email">My Email:</label>
+          <input name="email" id="email" value="<?php $user->htmlspecialwrite('email'); ?>">
+          <label for="photo">My Picture:</label>
+          <input name="img" type="file" id="img">
+          <?php if ($img = $user->files()->contains('file_name','img')) { ?>
+<div id="file<?php echo $img->id; ?>" class="file">
+				  <a href="<?php echo $img->original_file; ?>"><img src="<?php $img->write('thumbnail'); ?>" /></a>
+				  <a href="#deleteFile" data-file="<?php echo $img->id;?>">Delete</a>
+			  </div>
 				<?php } ?>
 			</p>
 	
 			<!-- These are meta fields.  They don't exist in the real user table, but the values will show up as if they did! -->
-			<p class="input"><label for="aboutme">About Me:</label>
-			<textarea name="aboutme" class="text" id="aboutme" wrap="virtual"><?php $user->htmlspecialwrite('aboutme'); ?></textarea></p>
-				
-			<p class="input"><label for="tagline">My Page Title:</label>
-			<input class="text" name="tagline" id="tagline" value="<?php $user->htmlspecialwrite('tagline'); ?>" /></p>
-
-			<p class="input"><label for="age">Age:</label>
-			<input class="text" name="age" id="age" length="5" maxlength="5" value="<?php $user->htmlspecialwrite('age'); ?>" /></p>
-
-			<p class="input"><label for="sex">Sex:</label>
-			<input class="text" name="sex" id="sex" maxlength="20" value="<?php $user->htmlspecialwrite('sex'); ?>" /></p>
-
-			<p class="input"><label for="location">Location:</label>
-			<input class="text" name="location" maxlength="100" id="location" value="<?php $user->htmlspecialwrite('location'); ?>" /></p>
+			<label for="aboutme">About Me:</label>
+          <textarea name="aboutme" id="aboutme" wrap="virtual"><?php $user->htmlspecialwrite('aboutme'); ?>
+            </textarea>
+            <label for="tagline">My Page Title:</label>
+            <input name="tagline" id="tagline" value="<?php $user->htmlspecialwrite('tagline'); ?>" />
+            <label for="age">Age:</label>
+            <input name="age" id="age" length="5" maxlength="5" value="<?php $user->htmlspecialwrite('age'); ?>" />
+            <label for="sex">Sex:</label>
+            <input name="sex" id="sex" maxlength="20" value="<?php $user->htmlspecialwrite('sex'); ?>" />
+            <label for="location">Location:</label>
+            <input name="location" maxlength="100" id="location" value="<?php $user->htmlspecialwrite('location'); ?>" />
+            <label for="homepage">My Homepage:</label>
+            <input name="homepage" id="homepage" value="<?php $user->htmlspecialwrite('homepage'); ?>" />
+            <hr />
+            
+			<h3>My role in the network:</h3>
+            <h4><?php $user->htmlspecialwrite( 'role' ); ?></h4>
+            
+            <hr />
+          <label>My Phone number:</label>
+            <input class="url text" name="meta_phone" value="<?php $user->htmlspecialwrite('phone'); ?>" />
+          <label>My chosen safeword:</label>
+            <input name="safeword" value="<?php $user->htmlspecialwrite('safeword'); ?>" />
+            <?php if( $user->role == 'healer' ){ ?>
+	      <label>My licensing allows me to:</label>
+            <input name="license_type"  value="<?php $user->htmlspecialwrite('license_type'); ?>" />
+          <label>My license identification number is:</label>
+            <input name="license" value="<?php $user->htmlspecialwrite('license'); ?>" />
+          <label>My state of licensing is:</label>
+            <input name="state_issuer_of_license" value="<?php $user->htmlspecialwrite('state_issuer_of_license'); ?>" />
+            <?php } ?>
+		  <!-- end meta fields -->		
 		
-			<p class="input"><label for="homepage">My Homepage:</label>
-			<input class="url text" name="homepage" id="homepage" value="<?php $user->htmlspecialwrite('homepage'); ?>" /></p>
-
-			<!-- end meta fields -->		
-		
-			<p class="input"><label>&nbsp;</label><input type="submit" class="button" value="Update my account" /></p>
+		  <label>&nbsp;</label>
+		  <button type="submit" class="btn"  >Update my account</button>
 		</form>
 		
 
@@ -72,9 +83,9 @@
 
 			<h3>Change My Password</h3>
 		
-			<p class="input"><label for="password">New Pass:</label><input name="password" id="password" type="password" class="text required" /></p>
+			<label for="password">New Pass:</label><input name="password" id="password" type="password" class="text required" />
 		
-			<p class="input"><label>&nbsp;</label><input class="button" type="submit" value="Set New Password" /></p>	
-	
+			<label>&nbsp;</label>
+			<input class="button" type="submit" value="Set New Password" />
 		</form>
 	</div>
